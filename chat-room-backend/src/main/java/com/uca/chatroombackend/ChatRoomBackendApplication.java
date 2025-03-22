0d0a7147f33263b93b50cccb77ddb1a3189ef2e8
Ghoutani Hicham <hichamghoutani52@gmail.com>
2025-03-22 10:44:26 +0000

    Initialisation du backend & frontend

---
+++ chat-room-backend/src/main/java/com/uca/chatroombackend/ChatRoomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uca.chatroombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomBackendApplication.class, args);
+	}
+
+}
---
+++ chat-room-backend/src/test/java/com/uca/chatroombackend/ChatRoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.chatroombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
