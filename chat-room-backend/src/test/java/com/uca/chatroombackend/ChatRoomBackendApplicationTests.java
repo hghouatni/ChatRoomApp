959a5a359ae93b480d355ceacb6f84389a1dd655
Ghoutani Hicham <hichamghoutani52@gmail.com>
2025-04-25 19:38:29 +0100

    Fix the issues

--- chat-room-backend/src/test/java/com/uca/chatroombackend/ChatRoomBackendApplicationTests.java
+++ chat-room-backend/src/test/java/com/uca/chatroombackend/ChatRoomBackendApplicationTests.java
@@ -2,12 +2,12 @@ package com.uca.chatroombackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChatRoomBackendApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
